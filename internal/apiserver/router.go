5e728c05c8032f99bbd16112d48005ab225e4c18
Nikita Mishagin <nikitamishagin@gmail.com>
2024-12-08 00:01:51 +0400

    Refactor router setup and enhance API endpoints.
    Consolidated router setup into a dedicated function to improve code organization. Added new endpoints for "/healthz", "/announces/v1beta", and "/service/v1beta" to extend the API functionality, providing status and feature-specific responses.

--- internal/apiserver/router.go
+++ internal/apiserver/router.go
@@ -3,13 +3,7 @@ package apiserver
 import "github.com/gin-gonic/gin"
 
 func NewAPIServer() error {
-	router := gin.Default()
-
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	router := setupRouter()
 
 	err := router.Run(":8080")
 	if err != nil {
@@ -17,4 +11,26 @@ func NewAPIServer() error {
 	}
 
 	return nil
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+
+	router.GET("/healthz", func(c *gin.Context) {
+		c.String(200, "ok")
+	})
+
+	router.GET("/announces/v1beta", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "announces v1beta endpoint",
+		})
+	})
+
+	router.GET("/service/v1beta", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "service v1beta endpoint",
+		})
+	})
+
+	return router
 }
