f30d41202d9fcc796c0e0c31fc2bd0af3a240286
Nikita Mishagin <nikitamishagin@gmail.com>
2024-12-24 00:22:58 +0400

    Add ListProjects method to EtcdClient
    Introduce a new ListProjects method to retrieve project data from etcd with a specified prefix. Includes a TODO comment indicating this is part of an incomplete feature for listing methods.

--- internal/apiserver/etcd.go
+++ internal/apiserver/etcd.go
@@ -90,6 +90,19 @@ func (e *EtcdClient) Get(key string) (string, error) {
 	return value, nil
 }
 
+// TODO: Complete list methods
+
+func (e *EtcdClient) ListProjects() ([]string, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	resp, err := e.client.Get(ctx, "/projects", clientv3.WithPrefix())
+	if err != nil {
+		return nil, fmt.Errorf("failed to get data from etcd: %w", err)
+	}
+	return resp, nil
+}
+
 func (e *EtcdClient) Delete(key string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
