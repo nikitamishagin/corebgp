0cb6985a3ea05a529c1cc20276e5379b68693e39
Nikita Mishagin <nikitamishagin@gmail.com>
2024-12-25 21:18:08 +0400

    Add List method to DatabaseAdapter interface
    The new List method allows retrieving multiple values associated with a key as a slice of strings. This enhances the interface's functionality and supports broader use cases for key-value operations.

--- internal/model/database.go
+++ internal/model/database.go
@@ -6,6 +6,7 @@ import clientv3 "go.etcd.io/etcd/client/v3"
 type DatabaseAdapter interface {
 	HealthCheck() error
 	Get(key string) (string, error)
+	List(key string) ([]string, error)
 	Put(key, value string) error
 	Patch(key, value string) error
 	Watch(key string, stopChan <-chan struct{}) (<-chan clientv3.WatchResponse, error)
