c0d88a8e812d22f82f9d5399f9cf248b63494a32
Nikita Mishagin <nikitamishagin@gmail.com>
2024-12-27 20:00:39 +0400

    Refactor updater package to main entry point
    Converted the updater package into a standalone executable by changing the package to "main" and adding an entry point function. This enables the updater to run directly as a CLI application.

--- cmd/updater/main.go
+++ cmd/updater/main.go
@@ -1,1 +1,14 @@
-package updater
+package main
+
+import (
+	"github.com/nikitamishagin/corebgp/internal/updater"
+	"log"
+)
+
+// main is the entry point of the application that starts the CoreBGP updater.
+func main() {
+	err := updater.RootCmd().Execute()
+	if err != nil {
+		log.Fatalf("failed to run updater: %v", err)
+	}
+}
