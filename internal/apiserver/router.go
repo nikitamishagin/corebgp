b0f9fea5965b5da9a31ddedf2ef2bbc391f44dc7
Nikita Mishagin <nikitamishagin@gmail.com>
2025-01-11 00:33:23 +0400

    Refactor router setup and improve endpoint documentation
    Updated `setupRouter` function to clarify its responsibilities and added detailed comments for each API endpoint. This improves code readability and ensures consistent understanding of route functionality for maintainers.

--- internal/apiserver/router.go
+++ internal/apiserver/router.go
@@ -19,12 +19,13 @@ func NewAPIServer(databaseAdapter model.DatabaseAdapter) error {
 	return nil
 }
 
-// setupRouter initializes and returns a new Gin Engine with predefined routes for health checks and API endpoints.
+// setupRouter initializes a Gin engine, sets up API routes and handlers, and configures health check and API endpoints.
 func setupRouter(db model.DatabaseAdapter) *gin.Engine {
 	router := gin.Default()
 
 	v1Handler := v1.NewHandler(db)
 
+	// Health check endpoint to verify if the service and database (etcd) are operational
 	router.GET("/healthz", func(c *gin.Context) {
 		// Check connection to etcd
 		if err := db.HealthCheck(); err != nil {
@@ -34,26 +35,34 @@ func setupRouter(db model.DatabaseAdapter) *gin.Engine {
 		c.String(http.StatusOK, "ok")
 	})
 
+	// Group all v1 API endpoints under /v1
 	v1 := router.Group("/v1")
 
+	// Retrieve a paginated list of announcements
 	v1.GET("/announcements/", v1Handler.ListAnnouncements)
 
+	// Retrieve all announcements without pagination
 	v1.GET("/announcements/all", v1Handler.GetAnnouncements)
 
+	// Retrieve a paginated list of announcements for a specific project
 	v1.GET("/announcements/:project/", v1Handler.ListAnnouncementsByProject)
 
+	// Retrieve all announcements for a specific project without pagination
 	v1.GET("/announcements/:project/all", v1Handler.GetAnnouncementsByProject)
 
+	// Retrieve a specific announcement by project and name
 	v1.GET("/announcements/:project/:name", v1Handler.GetAnnouncement)
 
-	// Write routes
+	// Create a new announcement
 	v1.POST("/announcements/", v1Handler.PostAnnouncement)
 
+	// Update specific fields of announcements
 	v1.PATCH("/announcements/", v1Handler.PatchAnnouncements)
 
-	// Route for watching announcements
+	// Watch announcements for real-time updates
 	v1.GET("/watch/announcements/", v1Handler.WatchAnnouncements)
 
+	// Delete a specific announcement by project and name
 	v1.DELETE("/announcements/:project/:name", v1Handler.DeleteAnnouncement)
 
 	return router
