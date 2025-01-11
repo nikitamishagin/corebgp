968f17a386cfae16c139c473d0ea36d93263c9b3
Nikita Mishagin <nikitamishagin@gmail.com>
2025-01-11 14:09:31 +0400

    Add CheckerHealth struct to track Checker component health
    Introduced the CheckerHealth struct to represent the health status of the Checker component across zones. It includes fields for the zone and the timestamp of the last response, aiding in monitoring and debugging.

--- internal/model/components.go
+++ internal/model/components.go
@@ -5,3 +5,9 @@ type Components struct {
 	Checker    bool `json:"checker"`
 	IPAMPlugin bool `json:"ipam-plugin"`
 }
+
+// CheckerHealth represents the health status of the Checker component responsible for a specific zone.
+type CheckerHealth struct {
+	Zone             string `json:"zone"`               // Zone represents the specific geographical zone or region associated with the health check.
+	LastResponseTime string `json:"last-response-time"` // LastResponseTime indicates the timestamp of the most recent response received from the health check.
+}
