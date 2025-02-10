84fe3574ac2a6aa42154b1b465423986e1313a45
Nikita Mishagin <n.mishagin@rambler-co.ru>
2025-02-11 01:06:59 +0400

    Refactor route management logic and improve synchronization.
    Consolidated and streamlined route handling by removing redundant code and introducing structured channel usage with `routeUpdates`. Improved connectivity monitoring and synchronization processes to enhance maintainability and performance. Simplified event handling to avoid unnecessary complexity.

--- internal/updater/cmd.go
+++ internal/updater/cmd.go
@@ -21,10 +21,6 @@ func RootCmd() *cobra.Command {
 			ctx, cancel := context.WithCancel(cmd.Context())
 			defer cancel()
 
-			// Create channels for routes
-			apiRoutesChan := make(chan []Route, 1)
-			controllerRoutesChan := make(chan []Route, 1)
-
 			for {
 				// Initialize the new GoBGP client
 				goBGPClient, err := NewGoBGPClient(&config.GoBGPEndpoint, &config.GoBGPCACert, &config.GoBGPClientCert, &config.GoBGPClientKey)
@@ -36,73 +32,27 @@ func RootCmd() *cobra.Command {
 				defer goBGPClient.Close()
 
 				// TODO: Implement GoBGP configuration checking
-				_, err = goBGPClient.GetBGP()
-				if err != nil {
-					fmt.Printf("Failed to get GoBGP configuration: %v. Retrying...\n", err)
-					time.Sleep(5 * time.Second) // Retry interval
-					continue
-				}
-
-				// For debug ListPath output
-				//routes, err := goBGPClient.ListPath([]string{"192.168.1.1/32"})
-				//if err != nil {
-				//	log.Fatalf("Failed to list paths: %v", err)
-				//}
-
-				//fmt.Println("Output from CMD:")
-				//fmt.Printf("Routes: %v\n", routes)
 
 				// Initialize the CoreBGP API client
 				apiClient := v1.NewAPIClient(&config.APIEndpoint, time.Second*5)
 
-				// Check if CoreBGP API server is healthy
-				err = apiClient.HealthCheck(ctx)
-				if err != nil {
-					fmt.Printf("Failed to connect to CoreBGP API: %v. Retrying...\n", err)
-					time.Sleep(5 * time.Second) // Retry interval
-					continue
-				}
+				// Periodically check connectivity while the program is running
+				go monitorConnectivity(ctx, goBGPClient, apiClient, cancel)
 
-				// Launch synchronization logic when both APIs are reachable
 				fmt.Println("Connections established. Starting synchronization...")
+
+				// Create a channel to process routeUpdates
+				routeUpdates := make(chan RouteUpdate, 100) // Buffered channel to handle updates
+				defer close(routeUpdates)
+
+				go watchAnnouncements(ctx, cancel, apiClient, routeUpdates)
+
+				// Create channels for routes
+				apiRoutesChan := make(chan map[string]Route, 1)
+				controllerRoutesChan := make(chan map[string]Route, 1)
 
 				// Create a WaitGroup to manage goroutines
 				var wg sync.WaitGroup
-
-				// Create a channel to process events
-				events := make(chan model.Event, 100) // Buffered channel to handle bursts of events
-				defer close(events)
-
-				// TODO: Improve events handling and move to separate function
-				// Goroutine for watching announcements
-				wg.Add(1) // Increment the WaitGroup counter
-				go func(ctx context.Context, cancel context.CancelFunc) {
-					defer wg.Done() // Decrement the WaitGroup counter when the goroutine ends
-
-					fmt.Println("Starting to watch announcements...")
-					err := apiClient.WatchAnnouncements(ctx, func(event model.Event) {
-						// Push each incoming event into the channel
-						events <- event
-					})
-					if err != nil {
-						fmt.Printf("Error while watching announcements: %v\n", err)
-						cancel() // Cancel the context in case of an error
-					}
-				}(ctx, cancel) // Pass both context and cancel as arguments
-
-				// Goroutine for processing events from the channel
-				wg.Add(1) // Increment the WaitGroup counter
-				go func() {
-					defer wg.Done() // Ensure the WaitGroup counter is decremented after processing ends
-					for event := range events {
-						// Handle each event in a separate goroutine
-						go func(ev model.Event) {
-							if err := handleAnnouncementEvent(goBGPClient, &ev); err != nil {
-								fmt.Printf("Failed to process event: %v\n", err)
-							}
-						}(event)
-					}
-				}()
 
 				wg.Add(1)
 				go fetchAPIRoutes(ctx, &wg, apiClient, apiRoutesChan)
@@ -113,18 +63,12 @@ func RootCmd() *cobra.Command {
 				wg.Add(1)
 				go synchronizeRoutes(ctx, &wg, apiRoutesChan, controllerRoutesChan, goBGPClient)
 
-				// Graceful shutdown: Ensure events channel is closed when the context is done
-				go func() {
-					<-ctx.Done()  // Wait for context cancellation or deadline
-					close(events) // Close the channel to signal worker goroutines to stop
-				}()
-
-				// Wait for all goroutines to finish
-				fmt.Println("Updater is running. Performing tasks...")
+				// Wait for sync goroutines to finish
 				wg.Wait()
 
-				// Periodically check connectivity while the program is running
-				go monitorConnectivity(ctx, goBGPClient, apiClient, cancel)
+				go routesHanding(ctx, goBGPClient, routeUpdates)
+
+				fmt.Println("Updater is running. Performing tasks...")
 			}
 		},
 	}
