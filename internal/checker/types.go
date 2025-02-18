4bc7289fb4c0aed5ccf3db8285e766986ba95ab6
Nikita Mishagin <nikitamishagin@gmail.com>
2025-02-19 00:14:04 +0400

    Add HealthCheck struct to define service health check config
    Introduce a new `HealthCheck` struct in the `checker` package to configure health check parameters. This provides a clear and reusable way to define endpoint, method, timing, and timeout for health checks.

---
+++ internal/checker/types.go
@@ -0,0 +1,11 @@
+package checker
+
+// HealthCheck represents the configuration for performing health checks on a service or endpoint.
+type HealthCheck struct {
+	Path          string `json:"path"`     // Path specifies the endpoint to be used for the health check process.
+	Port          int    `json:"port"`     // Port specifies the port number to be used for the health check process.
+	Method        string `json:"method"`   // Method specifies the HTTP method to be used for the health check process.
+	CheckInterval int    `json:"interval"` // CheckInterval specifies the interval in seconds between consecutive health check attempts.
+	Timeout       int    `json:"timeout"`  // Timeout specifies the duration in milliseconds before a health check request times out.
+	Delay         int    `json:"delay"`    // Delay specifies the time in seconds to wait before initiating the first health check.
+}
