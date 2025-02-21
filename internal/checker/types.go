ae37f13dc780f0c54bceb3f17cf6ea763b0927d6
Nikita Mishagin <nikitamishagin@gmail.com>
2025-02-21 23:27:48 +0400

    Add NextHop field to HealthCheck struct
    The NextHop field specifies the address of the next hop for routing or forwarding traffic. This enhances the health check configuration by enabling more detailed traffic control options.

--- internal/checker/types.go
+++ internal/checker/types.go
@@ -2,6 +2,7 @@ package checker
 
 // HealthCheck represents the configuration for performing health checks on a service or endpoint.
 type HealthCheck struct {
+	NextHop       string `json:"nextHop"`  // NextHop specifies the address of the next hop for routing or forwarding traffic.
 	Path          string `json:"path"`     // Path specifies the endpoint to be used for the health check process.
 	Port          int    `json:"port"`     // Port specifies the port number to be used for the health check process.
 	Method        string `json:"method"`   // Method specifies the HTTP method to be used for the health check process.
